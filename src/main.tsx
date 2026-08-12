2624c21400bac704343afcae9da2c7e8c8c1b5b2
SZStanton <ssebastianbusiness@gmail.com>
2026-08-12 05:36:03 +0200

    update fonts with josefin and manrope

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+// Variable fonts, so every weight comes from one file each.
+import '@fontsource-variable/josefin-sans'
+import '@fontsource-variable/manrope'
 import './index.css'
 import App from './App.tsx'
 
